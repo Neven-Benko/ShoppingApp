4b681830775a2bb9df098d006e3551581b7875fc
Neven Benko <nbenko@mail.com>
2024-05-28 21:48:39 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+
+const addButton = document.getElementById("button");
+const itemInput = document.getElementById("input");
+
+addButton.addEventListener("click", () => {
+    console.log("button press")
+})
