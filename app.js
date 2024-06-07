3b70129d71848fc3f17e7a00e4f1c8dc972a41e0
Neven Benko <nbenko@mail.com>
2024-06-07 19:56:39 +0200

    connected app to Firebase

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js';
+import { getDatabase, ref, push, onValue, remove } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js';
 
+const appSettings = {
+    databaseURL: "https://shopping-list-database-d0086-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const itemsinDB = ref(database, "shopping-list");
+
+
+const inputField = document.getElementById("input");
 const addButton = document.getElementById("button");
-const itemInput = document.getElementById("input");
 
 addButton.addEventListener("click", () => {
-    console.log("button press")
+    let inputValue = inputField.value;
+    let listItem = document.createElement("li");
+
+    push(itemsinDB, inputValue);
+
+    console.log(`${inputValue} added`);
 })
